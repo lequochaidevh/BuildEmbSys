da0e5af9620f90f3d5326d7d998973439663f9b1
lequochaidevh <lequochaidevh@gmail.com>
2025-04-06 18:24:55 +0700

    add DroneArdui PrjTemp

--- stuPID_autotune/inc/mockwire.h
+++ stuPID_autotune/inc/mockwire.h
@@ -1,30 +1,32 @@
 #ifndef MOCKWIRE_H
 #define MOCKWIRE_H
+#include <iostream>
+#include <cstdio>
 
 class MockWire {
 public:
     void begin() {
         // Giả lập việc bắt đầu kết nối I2C
-        Serial.println("Wire.begin() called (mock).");
+        printf("Wire.begin() called (mock). \n");
     }
 
     void beginTransmission(uint8_t address) {
         // Giả lập việc bắt đầu truyền dữ liệu I2C
-        Serial.print("Wire.beginTransmission(");
-        Serial.print(address);
-        Serial.println(") called (mock).");
+        printf("Wire.beginTransmission(");
+        printf("%d", address);
+        printf(") called (mock).\n");
     }
 
     void write(uint8_t value) {
         // Giả lập việc ghi dữ liệu vào I2C
-        Serial.print("Wire.write(");
-        Serial.print(value);
-        Serial.println(") called (mock).");
+        printf("Wire.write(");
+        printf("%d", value);
+        printf(") called (mock). \n");
     }
 
     void endTransmission() {
         // Giả lập việc kết thúc truyền dữ liệu
-        Serial.println("Wire.endTransmission() called (mock).");
+        printf("Wire.endTransmission() called (mock). \n");
     }
 };
 
